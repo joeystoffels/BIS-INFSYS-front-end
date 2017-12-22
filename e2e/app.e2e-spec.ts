c4007a9d66504728acf97bbf1112307362119094
Nick Hartjes <info@nickhartjes.nl>
2017-12-22 17:06:47 +0100

    Added theme

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { InspiniaPage } from './app.po';
 
-describe('gameparadise App', () => {
-  let page: AppPage;
+describe('inspinia App', () => {
+  let page: InspiniaPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new InspiniaPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
