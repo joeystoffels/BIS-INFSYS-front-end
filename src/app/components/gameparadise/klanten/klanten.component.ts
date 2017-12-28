b83189322ba38cf4468451f4694920a777e7b6fb
Nick Hartjes <info@nickhartjes.nl>
2017-12-28 10:33:09 +0100

    Added working datagrid, with master detail

--- src/app/components/gameparadise/klanten/klanten.component.ts
+++ src/app/components/gameparadise/klanten/klanten.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { KlantenService } from '../../../services/data/klanten.service';
-import { DxDataGridModule } from 'devextreme-angular';
 import { Klant } from '../../../services/interfaces/klant';
 import DataSource from 'devextreme/data/data_source';
+import { DxDataGridModule, DxDataGridComponent } from 'devextreme-angular';
 
 @Component({
   selector: 'app-klanten',
@@ -10,8 +10,15 @@ import DataSource from 'devextreme/data/data_source';
   styleUrls: ['./klanten.component.css']
 })
 export class KlantenComponent {
-
     public klanten: any = {};
+
+    selectionChanged(e) {
+        console.log(e.currentSelectedRowKeys[0]['emailadres']);
+        
+        
+        e.component.collapseAll(-1);
+        e.component.expandRow(e.currentSelectedRowKeys[0]);
+    }
 
     constructor(
       private klantenService: KlantenService
