5a0b6675ccf2e653f0b0c837273e5366887d516c
Joey Stoffels <joey.stoffels@exe.energy>
2018-01-25 15:31:20 +0100

    change icon/menu order

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -4,19 +4,19 @@ export const navigation = [
     name: 'Menu'
   },
   {
-    name: 'Views',
-    url: '/opdracht7',
-    icon: 'icon-drop'
-  },
-  {
     name: 'Master-Detail',
     url: '/opdracht8',
-    icon: 'icon-drop'
+    icon: 'icon-arrow-right'
+  },
+  {
+    name: 'Views',
+    url: '/opdracht7',
+    icon: 'icon-arrow-right'
   },
   {
     name: 'Rapporten',
     url: '/opdracht9',
-    icon: 'icon-drop'
+    icon: 'icon-arrow-right'
   }
   ];
 
